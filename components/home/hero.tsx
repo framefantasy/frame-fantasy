3428e35d09e88c3deb9845f43d62652f10f5e9d5
jagnani73 <yashjagnani73@gmail.com>
2022-03-02 13:03:42 +0530

    feat(home): add hero

---
+++ components/home/hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className="h-screen pt-40 overflow-hidden w-full flex">
+      <img
+        src="/hero/preview.gif"
+        alt="Home Preview | Frame Fantasy"
+        className="w-full object-cover mt-auto"
+      />
+    </section>
+  );
+};
+
+export default Hero;
---
+++ components/home/home.tsx
@@ -0,0 +1,7 @@
+import { Hero } from "./";
+
+export const Home = () => {
+  return <Hero />;
+};
+
+export default Home;
