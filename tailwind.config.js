c4f108e0599d8f3d156248f74f2608e4e92b3641
jagnani73 <yashjagnani73@gmail.com>
2022-03-02 12:41:12 +0530

    feat: add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E9BC73",
+        secondary: "#1F1F1F",
+      },
+    },
   },
   plugins: [],
 };
