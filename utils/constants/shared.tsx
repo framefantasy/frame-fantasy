6d20ab9399de1c16ec0e6e5eaaa597846d59a1cf
jagnani73 <yashjagnani73@gmail.com>
2022-03-08 19:46:05 +0530

    docs: navbar copy

--- utils/constants/shared.tsx
+++ utils/constants/shared.tsx
@@ -2,37 +2,43 @@ import { NavItem } from "../interfaces/shared";
 
 export enum ROUTES {
   INDEX = "/",
+  WEDDINGS = "/weddings",
+  FASHION_PORTFOLIO = "/fashion-portfolio",
+  COMMERCIAL_FASHION = "/commercial-fashion",
+  CONCEPT_SHOOT = "/concept-shoot",
+  BABY_PHOTOSHOOT = "/baby-photoshoot",
+  EVENTS = "/events",
 }
 
 export const NAV_ITEMS: NavItem[] = [
   {
     content: "Wedding",
     external: false,
-    href: ROUTES.INDEX,
+    href: ROUTES.WEDDINGS,
   },
   {
     content: "Fashion Portfolio",
     external: false,
-    href: ROUTES.INDEX,
+    href: ROUTES.FASHION_PORTFOLIO,
   },
   {
     content: "Commercial Fashion",
     external: false,
-    href: ROUTES.INDEX,
+    href: ROUTES.COMMERCIAL_FASHION,
   },
   {
     content: "Concept Shoot",
     external: false,
-    href: ROUTES.INDEX,
+    href: ROUTES.CONCEPT_SHOOT,
   },
   {
     content: "Baby Photoshoot",
     external: false,
-    href: ROUTES.INDEX,
+    href: ROUTES.BABY_PHOTOSHOOT,
   },
   {
     content: "Events",
     external: false,
-    href: ROUTES.INDEX,
+    href: ROUTES.EVENTS,
   },
 ];
