c27449b0d8241b23d7531e7f882d53f6afdfa9e8
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2022-09-27 10:54:21 -0400

    feat: add mailer API

--- pages/api/v1/contact.ts
+++ pages/api/v1/contact.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { contactRequest, validateRequest } from "../../../src/email.schema";
+import { sendEmail } from "../../../src/email.service";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -10,12 +11,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       };
     await validateRequest(req, res);
     const { email, name, phone, question } = req.body as contactRequest;
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Thank you for your query! We'll get back to you soon!",
+    Promise.all([sendEmail({ name, email, phone, question })])
+      .then((_) => {
+        console.log("Email sent to:", email);
+      })
+      .catch((err) => {
+        console.error("ERROR: Sending email to", email);
+        console.error(err);
       });
+    res.status(200).json({
+      success: true,
+      message: "Thank you for your query! We'll get back to you soon!",
+    });
   } catch (err) {
     if (err?.statusCode) {
       res.status(err.statusCode).json({
