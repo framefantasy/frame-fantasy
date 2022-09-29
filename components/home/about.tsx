d99346a4878be3751fbdd66b452c96d465ae0a4e
jagnani73 <yashjagnani73@gmail.com>
2022-09-29 10:06:00 +0530

    chore: copy

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -11,32 +11,31 @@ const About = () => {
         </figure>
 
         <div className="xl:w-9/12 xl:px-12 text-white">
-          <div className="w-10/12 border-b-4 border-primary flex flex-col lg:flex-row lg:items-end text-white pb-4 mt-12 xl:mt-0 mb-12">
-            <h2 className="capitalize">Purbafalguni Paul</h2>
-            <span className="text-sm lg:ml-8 mt-4 lg:mt-0 font-medium">
-              a quote maybe
-            </span>
+          <h2 className="w-10/12 border-b-4 border-primary flex flex-col lg:flex-row lg:items-end text-white pb-4 mt-12 xl:mt-0 mb-8">
+            Purbafalguni Paul
+          </h2>
+
+          <div className="mb-4">
+            <h4 className="mb-2">My Story</h4>
+            <p>
+              In 2012, I moved to Mumbai to get my Master's Degree in Journalism
+              & Mass Communication along with Diploma in Photography. After
+              three years of working as an assistant at a major photography
+              studio and working under many well-known photographers, I started
+              Frame Fantasy in Nov 2017. Meanwhile, I have achieved many awards
+              in photography, worked with many NGOs, Work with ICICI Bank.
+            </p>
           </div>
 
-          <p className="mb-4">
-            Sit ullamco culpa exercitation aute. Culpa sunt duis consequat
-            reprehenderit. Duis deserunt ut in eiusmod fugiat labore duis et non
-            cillum non mollit ex. Non excepteur occaecat reprehenderit
-            incididunt. Irure sint voluptate sint est do sint reprehenderit
-            fugiat sunt qui magna incididunt veniam. Irure duis aliquip labore
-            ipsum aute ullamco ad minim excepteur in minim pariatur nisi labore.
-            Duis nulla ea tempor quis est ad do voluptate irure cupidatat non.
-          </p>
-
-          <p>
-            Eu esse ex consectetur labore deserunt est ullamco consequat
-            consequat dolore excepteur elit voluptate. Officia proident commodo
-            amet cillum. Voluptate ut consectetur enim proident mollit in sit
-            qui proident nulla excepteur est duis. Amet occaecat ea ipsum
-            deserunt. Laboris dolor minim do nulla. Enim officia officia duis
-            duis deserunt consequat nisi eiusmod eiusmod aliquip nostrud aliquip
-            magna.
-          </p>
+          <div>
+            <h4 className="mb-2">My Experience</h4>
+            <p>
+              I have had the privilege to shoot many events and people. I am
+              experienced with many locations and photography styles. The
+              photograph style may differ from assignment to assignment, but my
+              commitment to quality and craft is unwavering. magna.
+            </p>
+          </div>
         </div>
       </div>
     </section>
